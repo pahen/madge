d9769421ad0aa8e5a3109158f33ce80ea926881b
Patrik Henningsson <patrik.henningsson@gmail.com>
2014-04-18 11:43:15 +0200

    Some code cleanup.

--- test/amd.js
+++ test/amd.js
@@ -4,11 +4,16 @@ var should = require('should'),
 describe('module format (AMD)', function () {
 
 	it('should behave as expected on ok files', function () {
-		madge([__dirname + '/files/amd/ok'], {format: 'amd'}).obj().should.eql({ a: [ 'sub/b' ], d: [], e: [ 'sub/c' ], 'sub/b': [ 'sub/c' ], 'sub/c': [ 'd' ] });
+		madge([__dirname + '/files/amd/ok'], {
+			format: 'amd'
+		}).obj().should.eql({ 'a': [ 'sub/b' ], 'd': [], 'e': [ 'sub/c' ], 'sub/b': [ 'sub/c' ], 'sub/c': [ 'd' ] });
 	});
 
 	it('should handle optimized files', function () {
-		madge([__dirname + '/files/amd/a-built.js'], {format: 'amd', optimized: true}).obj().should.eql({ a: [ 'sub/b' ], d: [], 'sub/b': [ 'sub/c' ], 'sub/c': [ 'd' ] });
+		madge([__dirname + '/files/amd/a-built.js'], {
+			format: 'amd',
+			optimized: true
+		}).obj().should.eql({ 'a': [ 'sub/b' ], 'd': [], 'sub/b': [ 'sub/c' ], 'sub/c': [ 'd' ] });
 	});
 
 	it('should merge in shim dependencies found in RequireJS config', function () {
@@ -22,44 +27,60 @@ describe('module format (AMD)', function () {
 		madge([__dirname + '/files/amd/ok'], {
 			format: 'amd',
 			exclude: '^sub'
-		}).obj().should.eql({ a: [], d: [], e: [] });
+		}).obj().should.eql({ 'a': [], 'd': [], 'e': [] });
 
 		madge([__dirname + '/files/amd/ok'], {
 			format: 'amd',
 			exclude: '.*\/c$'
-		}).obj().should.eql({ a: [ 'sub/b' ], d: [], e: [], 'sub/b': [] });
+		}).obj().should.eql({ 'a': [ 'sub/b' ], 'd': [], 'e': [], 'sub/b': [] });
 	});
 
 	it('should tackle errors in files', function () {
-		madge([__dirname + '/files/amd/error.js'], {format: 'amd'}).obj().should.eql({ error: [] });
+		madge([__dirname + '/files/amd/error.js'], {
+			format: 'amd'
+		}).obj().should.eql({ error: [] });
 	});
 
 	it('should handle id different than file', function () {
-		madge([__dirname + '/files/amd/namedWrapped/diff.js'], {format: 'amd'}).obj().should.eql({ ffid: [] });
+		madge([__dirname + '/files/amd/namedWrapped/diff.js'], {
+			format: 'amd'
+		}).obj().should.eql({ 'ffid': [] });
 	});
 
 	it('should handle named modules', function () {
-		madge([__dirname + '/files/amd/namedWrapped/car.js'], {format: 'amd'}).obj().should.eql({ car: [ 'engine', 'wheels' ] });
+		madge([__dirname + '/files/amd/namedWrapped/car.js'], {
+			format: 'amd'
+		}).obj().should.eql({ 'car': [ 'engine', 'wheels' ] });
 	});
 
 	it('should find circular dependencies', function () {
-		madge([__dirname + '/files/amd/circular'], {format: 'amd'}).circular().getArray().should.eql([ ['a', 'c'], ['f', 'g', 'h'] ]);
+		madge([__dirname + '/files/amd/circular'], {
+			format: 'amd'
+		}).circular().getArray().should.eql([ ['a', 'c'], ['f', 'g', 'h'] ]);
 	});
 
 	it('should find modules that depends on another', function () {
-		madge([__dirname + '/files/amd/ok'], {format: 'amd'}).depends('sub/c').should.eql([ 'e', 'sub/b' ]);
+		madge([__dirname + '/files/amd/ok'], {
+			format: 'amd'
+		}).depends('sub/c').should.eql([ 'e', 'sub/b' ]);
 	});
 
 	it('should compile coffeescript on-the-fly', function () {
-		madge([__dirname + '/files/amd/coffeescript'], {format: 'amd'}).obj().should.eql({ a: ['b'], b: [] });
+		madge([__dirname + '/files/amd/coffeescript'], {
+			format: 'amd'
+		}).obj().should.eql({ 'a': ['b'], 'b': [] });
 	});
 
 	it('should resolve relative module indentifiers', function () {
-		madge([__dirname + '/files/amd/relative'], {format: 'amd'}).obj().should.eql({ a: [], b: [ 'a' ], 'foo/bar/d': [ 'a' ], 'foo/c': [ 'a' ] });
+		madge([__dirname + '/files/amd/relative'], {
+			format: 'amd'
+		}).obj().should.eql({ 'a': [], 'b': [ 'a' ], 'foo/bar/d': [ 'a' ], 'foo/c': [ 'a' ] });
 	});
 
 	it('should ignore plugins', function () {
-		madge([__dirname + '/files/amd/plugin.js'], {format: 'amd', breakOnError: true}).obj().should.eql({ plugin: [ 'ok/a' ] });
+		madge([__dirname + '/files/amd/plugin.js'], {
+			format: 'amd',
+			breakOnError: true
+		}).obj().should.eql({ plugin: [ 'ok/a' ] });
 	});
-
 });
--- test/cjs.js
+++ test/cjs.js
@@ -4,45 +4,56 @@ var should = require('should'),
 describe('module format (CommonJS)', function () {
 
 	it('should behave as expected on ok files', function () {
-		madge([__dirname + '/files/cjs/normal']).obj().should.eql({ a: [ 'sub/b' ], d: [], 'sub/b': [ 'sub/c' ], 'sub/c': [ 'd' ] });
+		madge([__dirname + '/files/cjs/normal'])
+			.obj().should.eql({ 'a': [ 'sub/b' ], 'd': [], 'sub/b': [ 'sub/c' ], 'sub/c': [ 'd' ] });
 	});
 
 	it('should handle expressions in require call', function () {
-		madge([__dirname + '/files/cjs/both.js']).obj().should.eql({ both: [ 'node_modules/a', 'node_modules/b' ] });
+		madge([__dirname + '/files/cjs/both.js'])
+			.obj().should.eql({ 'both': [ 'node_modules/a', 'node_modules/b' ] });
 	});
 
 	it('should handle require call and chained functions', function () {
-		madge([__dirname + '/files/cjs/chained.js']).obj().should.eql({ chained: [ 'node_modules/a', 'node_modules/b', 'node_modules/c' ] });
+		madge([__dirname + '/files/cjs/chained.js'])
+			.obj().should.eql({ 'chained': [ 'node_modules/a', 'node_modules/b', 'node_modules/c' ] });
 	});
 
 	it('should handle nested require call', function () {
-		madge([__dirname + '/files/cjs/nested.js']).obj().should.eql({ nested: [ 'node_modules/a', 'node_modules/b', 'node_modules/c' ] });
+		madge([__dirname + '/files/cjs/nested.js'])
+			.obj().should.eql({ 'nested': [ 'node_modules/a', 'node_modules/b', 'node_modules/c' ] });
 	});
 
 	it('should handle strings in require call', function () {
-		madge([__dirname + '/files/cjs/strings.js']).obj().should.eql({ strings: [ 'events', 'node_modules/a', 'node_modules/b', 'node_modules/c', 'node_modules/doom', 'node_modules/events2', 'node_modules/y' ] });
+		madge([__dirname + '/files/cjs/strings.js'])
+			.obj().should.eql({ strings: [
+				'events', 'node_modules/a', 'node_modules/b', 'node_modules/c',
+				'node_modules/doom', 'node_modules/events2', 'node_modules/y'
+			]});
 	});
 
 	it('should tackle errors in files', function () {
-		madge([__dirname + '/files/cjs/error.js']).obj().should.eql({ error: [] });
+		madge([__dirname + '/files/cjs/error.js'])
+			.obj().should.eql({ 'error': [] });
 	});
 
 	it('should be able to exclude modules', function () {
 		madge([__dirname + '/files/cjs/normal'], {
 			exclude: '^sub'
-		}).obj().should.eql({ a: [], d: [] });
+		}).obj().should.eql({ 'a': [], 'd': [] });
 
 		madge([__dirname + '/files/cjs/normal'], {
 			exclude: '.*\/c$'
-		}).obj().should.eql({ a: [ 'sub/b' ], d: [], 'sub/b': [] });
+		}).obj().should.eql({ 'a': [ 'sub/b' ], 'd': [], 'sub/b': [] });
 	});
 
 	it('should find circular dependencies', function () {
-		madge([__dirname + '/files/cjs/circular']).circular().getArray().should.eql([ ['a', 'b', 'c'] ]);
+		madge([__dirname + '/files/cjs/circular'])
+			.circular().getArray().should.eql([ ['a', 'b', 'c'] ]);
 	});
 
 	it('should compile coffeescript on-the-fly', function () {
-		madge([__dirname + '/files/cjs/coffeescript']).obj().should.eql({ a: ['../node_modules/b'], b: [] });
+		madge([__dirname + '/files/cjs/coffeescript'])
+			.obj().should.eql({ 'a': ['../node_modules/b'], 'b': [] });
 	});
 
 });
