b475d27343d1490e156a8256e2dc74c1880d20cb
Brandon Selway <brandon.selway@gmail.com>
2014-05-20 13:55:05 -0700

    better test (include amd, and cleaner)

--- test/pluggable.js
+++ test/pluggable.js
@@ -1,21 +1,41 @@
 var should = require('should'),
+	path = require('path'),
 	madge = require('../lib/madge');
 
 describe('Madge', function () {
 	describe('pluggable', function () {
-		it('should serve parseFile and addModule events', function () {
+		it('should serve parseFile and addModule events for cjs', function () {
 			var fileAdd = "";
 			var idAdd = "";
 			var opts = {};
 			opts.onParseFile = function(obj){
-				fileAdd += obj.src;
+				var arr = obj.filename.split(path.sep);
+				fileAdd += arr[arr.length-1];
 			};
 			opts.onAddModule = function(obj){
 				idAdd += obj.id;
 			};
+			madge([__dirname + '/files/cjs/normal'], opts);
+			(fileAdd+idAdd).should.eql( "a.jsd.jsb.jsc.js" + "adsub/bsub/c" );
+		});
+	});
+
+	describe('pluggable - amd', function () {
+		it('should serve parseFile and addModule events for amd', function () {
+			var fileAdd = "";
+			var idAdd = "";
+			var opts = {};
+			opts.onParseFile = function(obj){
+				var arr = obj.filename.split(path.sep);
+				fileAdd += arr[arr.length-1];
+			};
+			opts.onAddModule = function(obj){
+				idAdd += obj.id;
+			};
+			opts.format = 'amd';
 			console.log(fileAdd)
-			madge([__dirname + '/files/cjs/multibase/1', __dirname + '/files/cjs/multibase/2'], opts);
-			(idAdd+fileAdd).should.eql( "1/a2/b" + "module.exports = 'A';module.exports = 'B';" );
+			madge([__dirname + '/files/amd/ok'], opts);
+			(idAdd+fileAdd).should.eql( "adesub/bsub/c" + "a.jsd.jse.jsb.jsc.js" );
 		});
 	});
 
@@ -28,8 +48,8 @@ describe('Madge', function () {
 				else
 					this.idAdd = ""+obj.id;
 			};
-			var madger = madge([__dirname + '/files/cjs/multibase/1', __dirname + '/files/cjs/multibase/2'], opts);
-			madger.idAdd.should.eql( "1/a2/b" );
+			var madger = madge([__dirname + '/files/cjs/normal'], opts);
+			madger.idAdd.should.eql( "adsub/bsub/c" );
 		});
 	});
 
