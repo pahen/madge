5deb37f062eb228f404910e8c6ceefb52986c6e4
Oli Lalonde <olalonde@gmail.com>
2016-06-28 08:25:31 -0700

    Support for es6 re-export syntax (#91)
    * Add support for re-export syntax
    
    e.g.:
    
    export { a } from './a'k
    export a from './a'
    export * as a from './a'
    
    * update npm-shrinkwrap.json
    
    * should should -> should, ^1.1.5 -> 1.1.5

--- test/es6.js
+++ test/es6.js
@@ -45,9 +45,15 @@ describe('module format (ES6)', function () {
     }).obj().should.eql({ 'jsx': [ 'absolute/b' ] });
   });
 
-  it('should should find imports on files with ES7', function() {
+  it('should find imports on files with ES7', function() {
     madge([__dirname + '/files/es6/async.js'], {
       format: 'es6'
     }).obj().should.eql({ 'async': [ 'absolute/b' ] });
+  });
+
+  it('should support export x from "./file"', function() {
+    madge([__dirname + '/files/es6/re-export'], {
+      format: 'es6'
+    }).obj().should.eql({ 'a': [], 'b-default': ['a'], 'b-named': ['a'], 'b-star': ['a'], 'c': ['b-default', 'b-named', 'b-star'] });
   });
 });
