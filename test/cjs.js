e89aa3dd2e8a19489e2af25c91aaa64cb1022d63
Patrik Henningsson <patrik.henningsson@gmail.com>
2016-07-25 16:19:30 +0200

    Use promises in API

--- test/cjs.js
+++ test/cjs.js
@@ -7,60 +7,81 @@ require('should');
 describe('CommonJS', () => {
 	const dir = __dirname + '/files/cjs';
 
-	it('should find recursive dependencies', () => {
-		madge(dir + '/normal/a.js').obj().should.eql({
-			'a': ['sub/b'],
-			'd': [],
-			'sub/b': ['sub/c'],
-			'sub/c': ['d']
-		});
+	it('should find recursive dependencies', (done) => {
+		madge(dir + '/normal/a.js').then((res) => {
+			res.obj().should.eql({
+				'a': ['sub/b'],
+				'd': [],
+				'sub/b': ['sub/c'],
+				'sub/c': ['d']
+			});
+			done();
+		}).catch(done);
 	});
 
-	it('should handle paths outside directory', () => {
-		madge(dir + '/normal/sub/c.js').obj().should.eql({
-			'../d': [],
-			'c': ['../d']
-		});
+	it('should handle paths outside directory', (done) => {
+		madge(dir + '/normal/sub/c.js').then((res) => {
+			res.obj().should.eql({
+				'../d': [],
+				'c': ['../d']
+			});
+			done();
+		}).catch(done);
 	});
 
-	it('should find circular dependencies', () => {
-		madge(dir + '/circular/a.js').circular().should.eql([
-			['a', 'b', 'c']
-		]);
+	it('should find circular dependencies', (done) => {
+		madge(dir + '/circular/a.js').then((res) => {
+			res.circular().should.eql([
+				['a', 'b', 'c']
+			]);
+			done();
+		}).catch(done);
 	});
 
-	it('should exclude core modules by default', () => {
-		madge(dir + '/core.js').obj().should.eql({
-			'core': []
-		});
+	it('should exclude core modules by default', (done) => {
+		madge(dir + '/core.js').then((res) => {
+			res.obj().should.eql({
+				'core': []
+			});
+			done();
+		}).catch(done);
 	});
 
-	it('should exclude NPM modules by default', () => {
-		madge(dir + '/npm.js').obj().should.eql({
-			'normal/d': [],
-			'npm': ['normal/d']
-		});
+	it('should exclude NPM modules by default', (done) => {
+		madge(dir + '/npm.js').then((res) => {
+			res.obj().should.eql({
+				'normal/d': [],
+				'npm': ['normal/d']
+			});
+			done();
+		}).catch(done);
 	});
 
-	it('should be able to include NPM modules', () => {
+	it('should be able to include NPM modules', (done) => {
 		madge(dir + '/npm.js', {
 			includeNpm: true
-		}).obj().should.eql({
-			'node_modules/a': [],
-			'normal/d': [],
-			'npm': ['node_modules/a', 'normal/d']
-		});
+		}).then((res) => {
+			res.obj().should.eql({
+				'node_modules/a': [],
+				'normal/d': [],
+				'npm': ['node_modules/a', 'normal/d']
+			});
+			done();
+		}).catch(done);
 	});
 
-	it('should be able to show file extensions', () => {
+	it('should be able to show file extensions', (done) => {
 		madge(dir + '/normal/a.js', {
 			showFileExtension: true
-		}).obj().should.eql({
-			'a.js': ['sub/b.js'],
-			'd.js': [],
-			'sub/b.js': ['sub/c.js'],
-			'sub/c.js': ['d.js']
-		});
+		}).then((res) => {
+			res.obj().should.eql({
+				'a.js': ['sub/b.js'],
+				'd.js': [],
+				'sub/b.js': ['sub/c.js'],
+				'sub/c.js': ['d.js']
+			});
+			done();
+		}).catch(done);
 	});
 
 });
--- test/madge.js
+++ test/madge.js
@@ -5,36 +5,67 @@ const madge = require('../lib/madge');
 require('should');
 
 describe('Madge', () => {
-	describe('#constructor', () => {
-		it('should throw error on missing filename argument', () => {
-			(() => {
-				madge();
-			}).should.throw('Filename argument is missing');
-		});
+	it('should throw error on missing filename argument', () => {
+		(() => {
+			madge();
+		}).should.throw('Filename argument is missing');
 	});
+
+	it('should return a Promise', () => {
+		madge(__dirname + '/files/cjs/a.js').should.be.Promise(); // eslint-disable-line new-cap
+	});
+
+	it('should throw error if filename argument is not a file', (done) => {
+		madge(__dirname + '/files').catch((err) => {
+			err.message.should.match(/is not a file/);
+			done();
+		}).catch(done);
+	});
+
+	it('should throw error if file does not exists', (done) => {
+		madge(__dirname + '/missing.js').catch((err) => {
+			err.message.should.match(/does not exists/);
+			done();
+		}).catch(done);
+	});
+
 	describe('#obj', () => {
-		it('should return dependency object', () => {
-			madge({a: ['b', 'c']}).obj().should.eql({a: ['b', 'c']});
+		it('should return dependency object', (done) => {
+			madge(__dirname + '/files/cjs/a.js').then((res) => {
+				res.obj().should.eql({
+					a: ['b', 'c'],
+					b: ['c'],
+					c: []
+				});
+				done();
+			}).catch(done);
 		});
 	});
 
 	describe('#dot', () => {
-		it('should be able to output graphviz DOT format', () => {
-			madge({
-				a: ['b', 'c'],
-				b: ['c'],
-				c: []
-			}).dot().should.eql('digraph G {\n  "a";\n  "b";\n  "c";\n  "a" -> "b";\n  "a" -> "c";\n  "b" -> "c";\n}\n');
+		it('should be able to output graphviz DOT format', (done) => {
+			madge(__dirname + '/files/cjs/b.js').then((res) => {
+				res.dot().should.eql('digraph G {\n  "b";\n  "c";\n  "b" -> "c";\n}\n');
+				done();
+			}).catch(done);
 		});
 	});
 
 	describe('#depends', () => {
-		it('should return modules that depends on another', () => {
-			madge({
-				a: ['b', 'c'],
-				b: ['c'],
-				c: []
-			}).depends('c').should.eql(['a', 'b']);
+		it('should return modules that depends on another', (done) => {
+			madge(__dirname + '/files/cjs/a.js').then((res) => {
+				res.depends('c').should.eql(['a', 'b']);
+				done();
+			}).catch(done);
+		});
+	});
+
+	describe('#image', () => {
+		it('should return a Promise', (done) => {
+			madge(__dirname + '/files/cjs/a.js').then((res) => {
+				res.image('c').should.be.Promise(); // eslint-disable-line new-cap
+				done();
+			}).catch(done);
 		});
 	});
 });
