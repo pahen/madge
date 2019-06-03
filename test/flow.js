139cf282071cb80cdbff04fa9b1c5d8884465235
Patrik Henningsson <patrik.henningsson@gmail.com>
2019-06-03 07:54:16 +0200

    Update test for mixed imports

--- test/flow.js
+++ test/flow.js
@@ -21,6 +21,16 @@ describe('Flow', () => {
 		madge(dir + '/cjs/calc.js').then((res) => {
 			res.obj().should.eql({
 				'geometry.js': [],
+				'calc.js': ['geometry.js']
+			});
+			done();
+		}).catch(done);
+	});
+
+	it('extracts CommonsJS module dependencies with mixed import syntax', (done) => {
+		madge(dir + '/cjs/calc.js', {detectiveOptions: {es6: {mixedImports: true}}}).then((res) => {
+			res.obj().should.eql({
+				'geometry.js': [],
 				'math.js': [],
 				'calc.js': ['geometry.js', 'math.js']
 			});
